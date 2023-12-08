029e14ebfbc2f94fdc1733d8615dafc770c67866
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-12-08 16:17:09 +0100

    jwt.rs : Verify

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -1,4 +1,5 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
+use jsonwebtoken::{decode, Algorithm, Validation, DecodingKey};
 
 pub enum Role {
     Access,
@@ -7,7 +8,31 @@ pub enum Role {
 /// Verify the validity of a JWT accordingly to its role (access or refresh)
 /// Return the email contained in the JWT if its valid
 /// Return an error if the JWT is invalid
-/// TODO Verify
+/// DONE Verify
 pub fn verify<T: Into<String>>(jwt: T, role: Role) -> Result<String> {
-    todo!()
+    let secret_key = "🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿🗿"; // TODO : Generate a clean key
+
+    let decoding_key = match role {
+        Role::Access => DecodingKey::from_secret(secret_key.as_ref()),
+        Role::Refresh => DecodingKey::from_secret(secret_key.as_ref()),
+    };
+
+    let validation = Validation {
+        algorithms: vec![Algorithm::HS256],
+        ..Validation::default()
+    };
+
+    match decode::<Claims>(
+        jwt.into().as_str(),
+        &decoding_key,
+        &validation,
+    ) {
+        Ok(token) => Ok(token.claims.email),
+        Err(err) => Err(anyhow!("JWT verification failed: {}", err)),
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct Claims {
+    email: String
 }

ece1d38b74fd5e4d8637d395b039bd0778f8edc1
Kylian <42773865+kylianmanzini@users.noreply.github.com>
2023-12-08 15:19:02 +0100

    added todo

--- src/utils/jwt.rs
+++ src/utils/jwt.rs
@@ -7,6 +7,7 @@ pub enum Role {
 /// Verify the validity of a JWT accordingly to its role (access or refresh)
 /// Return the email contained in the JWT if its valid
 /// Return an error if the JWT is invalid
+/// TODO Verify
 pub fn verify<T: Into<String>>(jwt: T, role: Role) -> Result<String> {
     todo!()
 }
